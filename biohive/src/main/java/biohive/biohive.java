0a84b7369c6f0c4063df2ac0879ce6bdabc57923
Sashidhar Jakkamsetti <sashidhar.jakkamsetti@gmail.com>
2019-04-19 14:08:01 -0700

    code setup

---
+++ biohive/src/main/java/biohive/biohive.java
@@ -0,0 +1,13 @@
+package biohive;
+
+/**
+ * Hello world!
+ *
+ */
+public class biohive
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
